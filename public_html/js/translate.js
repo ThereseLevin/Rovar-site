1e79239e8d58055703903470cee29d4e595c7dfb
Therese Levin <therese.levin@outlook.com>
2018-10-10 10:21:49 +0200

    Comments for js created

---
+++ public_html/js/translate.js
@@ -0,0 +1,17 @@
+//Vad ska scriptet göra?
+/* Hämta ett värde från formuläret på index sidan. Kollar om det finns några konsonanter i strängen. Om det finns konsonanter, då ska den lägga på ett "o" och samma konsonant igen. Vokaler blir oförändrade. Till slut sammanställa ordet och skriva ut det på sidan. */
+
+//Hur ska scriptet utföra det?
+/*
+1. En array med konsonanter
+2. Hämta värdet från formuläret och stoppa i en variabel
+3. Kolla OM det finns konsonanter i strängen.
+4. OM INTE, gör ingenting med strängen.
+5. OM det finns,  lägg till "o" och sig själv, där det finns. ( t = ot, e = eo)
+6. Skapa strängen i en ny variabel
+7. Skriva ut den nya variablen i dokumentet
+
+*/
+
+// Vokaler A, e, i, o, u, y, å, ä och ö.
+// Konsonanter B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, X, Z.
