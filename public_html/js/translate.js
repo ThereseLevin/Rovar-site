ecd7433fc65ca73165514c5f38990a54a616cbe5
Therese Levin <therese.levin@outlook.com>
2018-10-10 13:03:11 +0200

    Comments for js created, array with constants and function getFormData added

--- public_html/js/translate.js
+++ public_html/js/translate.js
@@ -4,14 +4,38 @@
 //Hur ska scriptet utföra det?
 /*
 1. En array med konsonanter
-2. Hämta värdet från formuläret och stoppa i en variabel
-3. Kolla OM det finns konsonanter i strängen.
-4. OM INTE, gör ingenting med strängen.
-5. OM det finns,  lägg till "o" och sig själv, där det finns. ( t = ot, e = eo)
-6. Skapa strängen i en ny variabel
-7. Skriva ut den nya variablen i dokumentet
+2. Lyssna efter event input / knapp
+3. Hämta värdet från formuläret och stoppa i en variabel
+4. Kolla OM det finns konsonanter i strängen.
+5. OM INTE, gör ingenting med strängen.
+6. OM det finns,  lägg till "o" och sig själv, där det finns. ( t = ot, e = eo)
+7. Skapa strängen i en ny variabel
+8. Skriva ut den nya variablen i dokumentet, när besökaren klickat på knappen
 
 */
 
 // Vokaler A, e, i, o, u, y, å, ä och ö.
 // Konsonanter B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, X, Z.
+
+// 1. Array med alla konsonanter
+
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'v', 'x', 'z'];
+
+console.log(consonants);
+
+
+// 2. Hämta värde från formulär och lägg i en variabel
+
+// skapar funktionen getFormData
+function getFormData() {
+
+    // Skapar variablen formData. Hämtar värde från formulär
+    var formData = document.getElementById("formid").value;
+
+    //Hittar outputtext i dokumentet. Skriver in värdet från formData i outputtext.
+    document.getElementById("outputtext").innerHTML = formData;
+
+    //För att texten ska stanna på sidan, och inte ladda in en ny sida vid tryckning på submit.
+    event.preventDefault();
+}
+
